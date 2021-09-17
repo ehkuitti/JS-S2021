dc28cf3e6f869586dbd7d237afc06960a48131fa
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2021-09-17 09:37:25 +0300

    Lisätty esimerkkejä-kansio

---
+++ esimerkkejä/Finding elements from DOM.js
@@ -0,0 +1,20 @@
+const element = document.getElementByID("intro");
+
+const element = document.getElementsByTagName("p");
+
+const x = document.getElementsByClassName("intro");
+
+const x = document.querySelectorAll("p.intro");
+
+The following HTML objects (and object collections) are also accessible: 
+
+document.anchors
+document.body
+document.documentElement
+document.embeds
+document.forms
+document.head
+document.images
+document.links
+document.scripts
+document.title
